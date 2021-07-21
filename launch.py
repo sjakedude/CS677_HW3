8cf6396d8c4484aae66f9340e80643eb0d078b2a
Jake Stephens <sjakedude@gmail.com>
2021-07-20 21:43:25 -0400

    did question 2

--- launch.py
+++ launch.py
@@ -74,6 +74,49 @@ print("=====")
 # ====================
 x_train, x_test = np.split(df.sample(frac=1), 2)
 
+x_train_green = x_train.loc[x_train["Color"] == "+"]
+x_train_red = x_train.loc[x_train["Color"] == "-"]
+x_test_green = x_test.loc[x_test["Color"] == "+"]
+x_test_red = x_test.loc[x_test["Color"] == "-"]
 
-# seaborn.pairplot(x_train.loc[x_train["Color"] == "-"])
-# plt.show()
+seaborn.pairplot(x_train_green)
+plt.savefig("output/good_bills.pdf")
+plt.clf()
+seaborn.pairplot(x_train_red)
+plt.savefig("output/fake_bills.pdf")
+
+# Checking each row in testing set
+# 3 rules to be a good bill:
+# Rule 1: f1 > -4
+# Rule 2: f2 > -10
+# Rule 3: -4 > f3 > -6
+
+tp = 0
+fp = 0
+tn = 0
+fn = 0
+
+for index, row in x_test.iterrows():
+    # We are predicting a good bill if this is true
+    if row["f1"] > -4 and row["f2"] > -10 and -4 > row["f3"] > -6:
+        # Check if prediction is correct
+        if row["Color"] == "+":
+            tp += 1
+        else:
+            fp += 1
+    else:
+        if row["Color"] == "-":
+            tn += 1
+        else:
+            fn += 1
+
+tpr = tp / (tp + fn)
+tnr = tn / (tn + fp)
+
+accuracy = round(((tp + tn) / len(x_test)) * 100, 2)
+
+accuracy_table = pd.DataFrame(
+    {"tp": [tp], "fp": fp, "tn": [tn], "fn": [fn], "accuracy": accuracy, "tpr": [tpr], "tnr": [tnr]}
+)
+
+print(accuracy_table)
